0ce8713cd3726628e0354b9f08cf191e49ed6287
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-04 21:48:12 +0800

    Update choking.java

--- main/java/com/example/miniprojects/choking.java
+++ main/java/com/example/miniprojects/choking.java
@@ -3,6 +3,8 @@ package com.example.miniprojects;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebChromeClient;
 
 public class choking extends AppCompatActivity {
 
@@ -12,12 +14,7 @@ public class choking extends AppCompatActivity {
         setContentView(R.layout.activity_choking);
 
         WebView webView = findViewById(R.id.webView);
-        String video = "<iframe width="100%" height="100%" 
-            src="https://www.youtube.com/embed/j45WfhxK_Hs?si=-aZQRBCIzEx9vj7d" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
+        String video = "<iframe width="100%" height="100%" src="https://www.youtube.com/embed/j45WfhxK_Hs?si=-aZQRBCIzEx9vj7d" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
         webView.loadData(video, "text/html", "utf-8");
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
