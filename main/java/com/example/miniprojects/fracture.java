b62ed790b66d5b349a4626645f66585d6702a316
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-04 20:53:28 +0800

    Update fracture.java

--- main/java/com/example/miniprojects/fracture.java
+++ main/java/com/example/miniprojects/fracture.java
@@ -14,7 +14,8 @@ public class fracture extends AppCompatActivity {
         WebView webView = findViewById(R.id.webView);
         String video = "<iframe width="100%" height="100%" 
             src="https://www.youtube.com/embed/N-FY78--JWw?si=7NxNKGu3VoBysM_o" 
-            title="YouTube video player" frameborder="0" 
+            title="YouTube video player" 
+            frameborder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
         webView.loadData(video, "text/html", "utf-8");
