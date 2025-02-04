48dc4100c311ea32dedcc5e04beb6f1c90a71edf
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-04 20:51:26 +0800

    Update fainting.java
    fainting video

--- main/java/com/example/miniprojects/fainting.java
+++ main/java/com/example/miniprojects/fainting.java
@@ -10,5 +10,16 @@ public class fainting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fainting);
+
+        WebView webView = findViewById(R.id.webView);
+        String video = "<iframe width="100%" height="100%" 
+            src="https://www.youtube.com/embed/Mlp5dRIJk4M?si=4Awmp8D07aRnLHe6" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
+        webView.loadData(video, "text/html", "utf-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
     }
 }
