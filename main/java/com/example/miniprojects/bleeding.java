9d2f99aa276310c68d19ab1fa86bcb13a83c75d0
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-04 21:47:24 +0800

    Update bleeding.java
    import stuff

--- main/java/com/example/miniprojects/bleeding.java
+++ main/java/com/example/miniprojects/bleeding.java
@@ -3,6 +3,9 @@ package com.example.miniprojects;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebChromeClient;
 
 public class bleeding extends AppCompatActivity {
 
@@ -12,12 +15,7 @@ public class bleeding extends AppCompatActivity {
         setContentView(R.layout.activity_bleeding);
 
         WebView webView = findViewById(R.id.webView);
-        String video = "<iframe width="100%" height="100%" 
-            src="https://www.youtube.com/embed/NxO5LvgqZe0?si=iO3qRnT6QSEbI_zc" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
+        String video = "<iframe width="100%" height="100%" src="https://www.youtube.com/embed/NxO5LvgqZe0?si=iO3qRnT6QSEbI_zc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
         webView.loadData(video, "text/html", "utf-8");
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
