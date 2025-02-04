5c43398dc9c6ae9624871950f0db2db04a441c64
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-04 21:48:53 +0800

    Update fainting.java

--- main/java/com/example/miniprojects/fainting.java
+++ main/java/com/example/miniprojects/fainting.java
@@ -3,6 +3,8 @@ package com.example.miniprojects;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebChromeClient;
 
 public class fainting extends AppCompatActivity {
 
@@ -12,12 +14,7 @@ public class fainting extends AppCompatActivity {
         setContentView(R.layout.activity_fainting);
 
         WebView webView = findViewById(R.id.webView);
-        String video = "<iframe width="100%" height="100%" 
-            src="https://www.youtube.com/embed/Mlp5dRIJk4M?si=4Awmp8D07aRnLHe6" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
+        String video = "<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Mlp5dRIJk4M?si=4Awmp8D07aRnLHe6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
         webView.loadData(video, "text/html", "utf-8");
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
