91bdfc6432b74c770881ce2840ebee66fc0ce951
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-04 20:50:08 +0800

    Update fracture.java
    fracture video

--- main/java/com/example/miniprojects/fracture.java
+++ main/java/com/example/miniprojects/fracture.java
@@ -10,5 +10,15 @@ public class fracture extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fracture);
+
+        WebView webView = findViewById(R.id.webView);
+        String video = "<iframe width="100%" height="100%" 
+            src="https://www.youtube.com/embed/N-FY78--JWw?si=7NxNKGu3VoBysM_o" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
+        webView.loadData(video, "text/html", "utf-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
     }
 }
