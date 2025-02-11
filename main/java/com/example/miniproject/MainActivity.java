84463d564bde4f02714ec03b38f9334afea37875
acethekid <kirito1246@gmail.com>
2025-02-11 20:54:24 +0800

    Add files via upload

---
+++ main/java/com/example/miniproject/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnAcs, btnnrby, btnaddi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAcs = findViewById(R.id.btn_acs);   // button accessing
+        btnnrby = findViewById(R.id.btn_nrby); //button nearby hospitals/clinics
+        btnaddi = findViewById(R.id.btn_addi); //button additional resources
+
+        btnAcs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainAccessing.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
