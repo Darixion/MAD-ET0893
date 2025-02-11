8d1a16717abb0b8e63180f3639e897188e88345a
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-11 19:34:26 +0800

    Update bleeding.java
    bleeding video fix

--- main/java/com/example/miniprojects/bleeding.java
+++ main/java/com/example/miniprojects/bleeding.java
@@ -14,7 +14,7 @@ public class bleeding extends AppCompatActivity {
         setContentView(R.layout.activity_bleeding);
 
         WebView webView = findViewById(R.id.webView);
-        String video = "<iframe width="100%" height="100%" src="https://www.youtube.com/embed/NxO5LvgqZe0?si=iO3qRnT6QSEbI_zc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>";
+        String video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/V8KiNURVjgk?si=TloM0xLs_l_pLQf8\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
         webView.loadData(video, "text/html", "utf-8");
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
