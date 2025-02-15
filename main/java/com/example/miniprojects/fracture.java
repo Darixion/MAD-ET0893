c0c639a3377ebb055f81ac1d1353e662bc3f6c28
Afeefiv <149168720+Afeefiv@users.noreply.github.com>
2025-02-15 20:59:21 +0800

    Update fracture.java

--- main/java/com/example/miniprojects/fracture.java
+++ main/java/com/example/miniprojects/fracture.java
@@ -14,7 +14,7 @@ public class fracture extends AppCompatActivity {
         setContentView(R.layout.activity_fracture);
 
         WebView webView = findViewById(R.id.webView);
-        String video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/N-FY78--JWw?si=M5mvplg77N-ibfx2\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>>";
+        String video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/N-FY78--JWw?si=M5mvplg77N-ibfx2\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
         webView.loadData(video, "text/html", "utf-8");
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
